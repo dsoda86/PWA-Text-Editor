88fbe844251323711691353d025ee17863949fee
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-08-23 17:32:51 -0700

    PWA builds, runs, saves text. Header not loading and install button works but stays visible after installing app.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,20 +3,21 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the triggered events
-    window.deferredPrompt = event;
+  event.preventDefault();
+  // Store the triggered events
+  window.deferredPrompt = event;
 
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
+  // Remove the hidden class from the button.
+  butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+  const promptEvent = window.deferredPrompt;
 
-    if (!promptEvent) {
-    return;
-    }
+  if (!promptEvent) {
+  return;
+  }
 
   // Show prompt
   promptEvent.prompt();
